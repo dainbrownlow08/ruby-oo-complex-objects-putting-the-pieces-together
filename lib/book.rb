c37656ee09dc2152f67f8c66c0caac66f18a5da6
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-18 13:43:07 -0800

    this

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
+class Book
+    def initialize(title)
+        @title = title
+        @author
+        @page_count
+        @genre
+    end
+
+    def title
+        @title
+    end
+
+    def author
+        @author
+    end
+
+    def genre
+        @genre
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+    
+    def page_count=(num)
+        @page_count = num
+    end
+    
+    def author=(name)
+        @author = name
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+    
+
+end
+
+
+
 
 
